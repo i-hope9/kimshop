b2877eb41ca59128d1313749828efce2e74bd6ee
Eeejeong Kim <ennea9on@gmail.com>
2023-02-01 07:45:03 +0900

    initial commit: 프로젝트 구조

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import ProductDetail from "./pages/ProductDetail";
+import Products from "./pages/Products";
+import NewProduct from "./pages/NewProduct";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+    children: [
+      { index: true, element: <Products /> },
+      { path: "/products", element: <Products />},
+      { path: "/products/:productId", element: <ProductDetail /> },
+      { path: "/products/new", element: <NewProduct />},
+      { path: "/cart", element: <Cart />}
+    ]
+  }
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
